b1eaa27f19f48266f10f4cefcb42659503ef1ee6
Measured & Made <measuredandmade@outlook.com>
2025-12-31 17:41:39 -0500

    Finished Navbar - Burger Menu Working, Logo polished

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,27 +1,25 @@
 import React, { useState } from 'react';
 import '../css/navbar.css';
-import logo from '../assets/logo.png';
+import logo from '../assets/logo4.png';
 
 const Navbar = () => {
     const [isOpen, setIsOpen] = useState(false);
 
-    const toggleMenu = () => {
-        
-    }
+    
     return (
         <nav className='navbar' role='navigation' aria-label='main navigation'>
             <div className='navbar-brand'>
-                <a className='' href='/'>
+                <a className='mt-5' href='/'>
                     <img height='250' width='250' src={logo} alt='Measured & Made' />
                 </a>
-                <a className='navbar-burger' role='button' aria-label='menu' aria-expanded='false' data-target='mainNavigation'>
+                <a className={`navbar-burger ${isOpen ? 'is-active' : ''}`} onClick={() => {setIsOpen(!isOpen)}} role='button' aria-label='menu' aria-expanded='false' data-target='mainNavigation'>
                     <span aria-hidden='true'></span>
                     <span aria-hidden='true'></span>
                     <span aria-hidden='true'></span>
                     <span aria-hidden='true'></span>
                 </a>
             </div>
-            <div id='mainNavigation' className='navbar-menu'>
+            <div id='mainNavigation' className={`navbar-menu ${isOpen ? 'is-active' : ''}`}>
                 <div className='navbar-end  is-align-items-flex-end '>
                     <a className='navbar-item px-5'>Home</a>
                     <a className='navbar-item px-5'>About</a>
