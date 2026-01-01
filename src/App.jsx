7679564ba82222ef1e079555fcc2e803e52d5d00
Measured & Made <measuredandmade@outlook.com>
2026-01-01 14:54:23 -0500

    Hero section done

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,18 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './css/styles.css';
 import Navbar from './components/Navbar';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
 
 function App() {
   
   return (
-    <div className='container'>
+    <div className=''>
       <Navbar />
+
+      <Routes>
+        <Route path='/' element={ <Home /> } />
+      </Routes>
     </div>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,9 +7,9 @@ const Navbar = () => {
 
     
     return (
-        <nav className='navbar' role='navigation' aria-label='main navigation'>
+        <nav className='navbar mb-3' role='navigation' aria-label='main navigation'>
             <div className='navbar-brand'>
-                <a className='mt-5' href='/'>
+                <a className='mt-5 ml-6' href='/'>
                     <img height='250' width='250' src={logo} alt='Measured & Made' />
                 </a>
                 <a className={`navbar-burger ${isOpen ? 'is-active' : ''}`} onClick={() => {setIsOpen(!isOpen)}} role='button' aria-label='menu' aria-expanded='false' data-target='mainNavigation'>
@@ -19,8 +19,8 @@ const Navbar = () => {
                     <span aria-hidden='true'></span>
                 </a>
             </div>
-            <div id='mainNavigation' className={`navbar-menu ${isOpen ? 'is-active' : ''}`}>
-                <div className='navbar-end  is-align-items-flex-end '>
+            <div id='mainNavigation' className={`navbar-menu ${isOpen ? 'is-active' : ''} `}>
+                <div className='navbar-end mr-6 is-align-items-flex-end '>
                     <a className='navbar-item px-5'>Home</a>
                     <a className='navbar-item px-5'>About</a>
                     <a className='navbar-item px-5'>Portfolio</a>
