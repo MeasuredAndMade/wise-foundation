e10298c5bc202dedd7a9cdf6e1a2423db3a5b6ca
Measured & Made <measuredandmade@outlook.com>
2026-01-01 19:16:30 -0500

    Moved 'About Us' to reuseable file

--- src/components/home/MultiSection.jsx
+++ src/components/home/MultiSection.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../../css/multi.css';
 import Services from './Services';
-import img from '../../assets/ComingBG.png';
+import AboutUs from './AboutUs';
 
 const MultiSection = () => {
     return (
@@ -10,20 +10,7 @@ const MultiSection = () => {
                  <div className='column is-8 has-text-centered'>
                     <Services />
                     <hr className='services-hr' />
-                    <div className='columns'>
-                        <div className='column is-6'>
-                            <h2 className='title service-title has-text-left'>About Us</h2>
-                            <p className='service-descrip has-text-left'>I build purposeful websites designed to be reliable, scalable, and easy to maintain. My work focuses on custom development, streamlined launches, and thoughtful optimization—helping businesses get online, grow confidently, and make the most of their digital presence.
-                            <br /><br />
-                            Every project starts with understanding your goals and ends with a solution that’s built with intention, not excess.</p>
-                            <div className='has-text-left mt-3'>
-                                <a href='/' className='about-link'>Learn more about our approach →</a>
-                            </div>
-                        </div>
-                        <div className='column is-6 m-auto'>
-                            <img src={img} />
-                        </div>
-                    </div>
+                    <AboutUs />
                 </div>
                 <div className='column is-4 services-section'></div>
             </div>
